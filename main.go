e69ee15b5568c2a90244db676b5ffb10c681dfcc
asakhant <asakhantayev@gmail.com>
2024-08-31 14:34:37 +0500

    getField and getNumOfFields implemented

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+// import "strings"
+
+func main() {
+	// csvReader := strings.NewReader(
+	// 	"Year,Make,Model,Description,Price\r" +
+	// 		"1997,Ford,E350,\"ac, abs, moon\",3000.00\n" +
+	// 		"1999,Chevy,\"Venture \"\"Extended Edition\"\"\",\"\",4900.00")
+
+	// var newParser CSVParser
+	// newParser = new(Parser)
+}
